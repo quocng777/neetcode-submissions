db4843e8c403df8434a190b5ce6cc133cbf14177
Quoc Nguyen <88817280+quocng777@users.noreply.github.com>
2026-06-12 21:40:14 +0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        s = new String(chars);
+        chars = t.toCharArray();
+        Arrays.sort(chars);
+        t = new String(chars);
+        if (s.equals(t)) {
+            return true;
+        }
+        return false;
+    }
+}
